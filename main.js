7dd9b3129dc3326c6b3a35407496806dd696524c
Bushra-253 <nbushranoor520@gmail.com>
2026-06-05 18:18:46 +0500

    Initial commit

--- main.js
+++ main.js
@@ -47,10 +47,15 @@ app.use(
 }
   })
 );
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://jaysheen-purse-store.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 
 
@@ -76,6 +81,16 @@ app.listen(5001, () => {
     console.log('Server is running on port 5001');
 });
 
-mongoose.connect('mongodb://localhost:27017/product').then(()=>{
-    console.log('mongodb connect successfully')
-})
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://nbushranoor520_db_user:<db_password>@cluster0.vc6roq0.mongodb.net/?appName=Cluster0/produt'
+        );
+
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.log('MongoDB connection failed:', error);
+    }
+};
+
+connectDB();
