5ccc3ea74f5eaa203211e98ebff60572508484c6
Bushra-253 <nbushranoor520@gmail.com>
2026-06-10 17:52:20 +0500

    Update mail.js

--- confiq/mail.js
+++ confiq/mail.js
@@ -3,33 +3,22 @@ dotenv.config();
 
 import nodemailer from "nodemailer";
 
-console.log("HOST:", process.env.SMTP_HOST);
-console.log("PORT:", process.env.SMTP_PORT);
-console.log("USER:", process.env.SMTP_USER);
-
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-
   port: Number(process.env.SMTP_PORT),
-
   secure: false,
-
+  family: 4, // Force IPv4
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
 });
 
-// VERIFY CONNECTION
 transporter.verify((error, success) => {
-
   if (error) {
     console.log("MAIL ERROR");
     console.log(error);
-  } 
-  
-  else {
+  } else {
     console.log("MAIL SERVER IS READY");
   }
-
 });
